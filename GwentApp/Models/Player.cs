66b17edeae7d98e3602677e49e07406a611628d3
otmshank00 <will.sykes@csus.edu>
2015-08-13 16:19:57 -0700

    added ignored db

--- GwentApp/Models/Player.cs
+++ GwentApp/Models/Player.cs
@@ -37,29 +37,6 @@ namespace GwentApp.Models
                 return listitems;
             }
         }
-        public static List<SelectListItem> nkleaderlist
-        {
-            get
-            {
-                List<SelectListItem> leaderlist = new List<SelectListItem>();
-                leaderlist.Add(new SelectListItem() { Value = "NR", Text = "Foltest the steel forged" });
-                leaderlist.Add(new SelectListItem() { Value = "NR", Text = "Foltest Lord commander of the North" });
-                leaderlist.Add(new SelectListItem() { Value = "NR", Text = "Foltest King of Temeria" });
-                leaderlist.Add(new SelectListItem() { Value = "NR", Text = "Foltest the Siegemaster" });
-                return leaderlist;
-            }
-        }
-        public static List<SelectListItem> neleaderlist
-        {
-            get
-            {
-                List<SelectListItem> leaderlist = new List<SelectListItem>();
-                leaderlist.Add(new SelectListItem() { Value = "NE", Text = "Emhyr var Emreis His Imperial Majesty" });
-                leaderlist.Add(new SelectListItem() { Value = "NE", Text = "Emhyr var Emreis the Relentless" });
-                leaderlist.Add(new SelectListItem() { Value = "NE", Text = "Emhyr var Emreis Emperor of Nilfgaard" });
-                leaderlist.Add(new SelectListItem() { Value = "NE", Text = "Emhyr var Emreis the White Flame" });
-                return leaderlist;
-            }
-        }
+        
     }
 }
